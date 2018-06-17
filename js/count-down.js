e7269112a860c33e41e3766af7190080126a149f
daublej <jiangjiedeyx@163.com>
2018-06-17 16:03:40 +0800

    弹窗效果优化

--- js/count-down.js
+++ js/count-down.js
@@ -1,12 +1,12 @@
 $('.nav-bar .btn').click(function () {
-    var count = parseInt($('.model .count').html());        $('.model').show();
+    var count = parseInt($('.model .count i').html());        $('.model').show();
     var timer = setInterval(function () {
         count--;
         if(count===0){
             clearInterval(timer);
             $('.model').hide();
         }
-        $('.model .count').html(count);
+        $('.model .count i').html(count);
     },1000);
 
 });
